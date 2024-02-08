132dfd419b94f6f4d7bb2699ab6139154c345810
mbenfredj <benfredjmajdi@gmail.com>
2024-02-08 11:07:03 +0100

    exctration des données: Club et  Joueurs

--- src/FFTT_scraping.py
+++ src/FFTT_scraping.py
@@ -3,20 +3,85 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 DEST_FOLDER = "../FFTT_DATA"
-URL = "https://www.pongiste.fr/include/pages/clubs_dep.php"
-page = requests.get(URL)
+URL_DEPARTEMENT = "https://www.pongiste.fr/include/pages/clubs_dep.php"
+page = requests.get(URL_DEPARTEMENT)
 soup = BeautifulSoup(page.content, "html.parser")
 dep_table = soup.find(id="tab_dep")
 dep_elements = dep_table.find_all("tr")
 
-dep_result=[]
+# List Départements
+dep_result = []
 for dep_element in dep_elements:
     if dep_element.find("a"):
         # Récupération de l'ID du département
         dep_id = dep_element.find("a").text
         # Récupération du nom du département
         dep_name = dep_element.find_all("a")[1].text
-        dep_row=[dep_id,dep_name]
+        dep_row = [dep_id, dep_name]
         dep_result.append(dep_row)
 df_dep = pd.DataFrame(dep_result, columns=["ID", "Name"])
-df_dep.to_csv(DEST_FOLDER+"/departements.csv", sep="\t")
+csv_departement_file = DEST_FOLDER + "/departements.csv"
+df_dep.to_csv(csv_departement_file, sep="\t")
+print(f"#Département Fichier {csv_departement_file} créé avec succès.")
+
+# List Clubs par Département
+all_club = []
+for dep in dep_result:
+    dep_id = dep[0]
+    dep_name = dep[1]
+    URL_CLUB = "https://www.pongiste.fr/include/pages/clubs_dep.php?num_dep="
+    page = requests.get(URL_CLUB + dep_id)
+    soup = BeautifulSoup(page.content, "html.parser")
+    if (soup.find(id="tab_clubs_dep")):
+        club_table = soup.find(id="tab_clubs_dep")
+        club_elements = club_table.find_all("tr")
+        club_result = []
+        for club_element in club_elements:
+            if club_element.find("a"):
+                numero = club_element.find("a").text
+                nom = club_element.find_all("a")[1].text
+                date_validation = club_element.find_all("td")[2].text
+                club_row = [numero, nom, date_validation]
+                club_result.append(club_row)
+                all_club.append([numero, nom])
+        df_club = pd.DataFrame(club_result, columns=["Numero", "NOM", "Date validation"])
+        csv_club_file = DEST_FOLDER + "/clubs/" + dep_id + "_" + dep_name.replace("/", "_") + ".csv"
+        df_club.to_csv(csv_club_file)
+        print(f"#Club Fichier {csv_club_file} créé avec succès.")
+
+# List Joueurs
+
+URL_JOUEUR = 'https://www.pongiste.fr/include/pages/joueurs.php?num_club='
+REFERER_URL = 'https://www.pongiste.fr/'
+headers = {
+    'Referer': REFERER_URL,
+}
+
+for club in all_club:
+    club_num = club[0]
+    club_nom = club[1]
+    page = requests.get(URL_JOUEUR + club_num, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+    joueur_table = soup.find(id="tab_joueurs")
+    joueur_elements = joueur_table.find_all("tr")
+    joueurs_result = []
+    for joueur_element in joueur_elements[1:]:
+        nom = joueur_element.find_all("td")[0].text
+        prenom = joueur_element.find_all("td")[1].text
+        licence = joueur_element.find_all("td")[2].text
+        sexe = joueur_element.find_all("td")[3].text
+        pts_cls = joueur_element.find_all("td")[4].text
+        certif_medical = joueur_element.find_all("td")[5].text
+        cat_age = joueur_element.find_all("td")[6].text
+        type_licence = joueur_element.find_all("td")[7].text
+        joueurs_row = [nom, prenom, licence, sexe, pts_cls, certif_medical, cat_age, type_licence, club_num]
+        joueurs_result.append(joueurs_row)
+    df_joueurs = pd.DataFrame(joueurs_result,
+                              columns=["NOM", "Prénom", "Licence", "Sexe", "Pts Cls", "Certif. médical", "Cat. d'âge",
+                                       "Type licence", "Num club"])
+
+    csv_joueur_file = DEST_FOLDER + "/joueurs/" + club_nom.replace("/", "_").strip() + ".csv"
+    print ("******"+csv_joueur_file)
+    df_joueurs.to_csv(csv_joueur_file)
+    print(f"#Joueurs: Fichier {csv_joueur_file} créé avec succès.")
+
